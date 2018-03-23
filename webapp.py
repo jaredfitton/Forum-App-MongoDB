4d6b3eee0538aadd348c65e538b00a77865e08c1
Michael Moschitto <michaelmoschitto@gmail.com>
2018-03-23 09:59:35 -0700

    fixedspacing

--- webapp.py
+++ webapp.py
@@ -64,7 +64,7 @@ def posts_to_html():
             # print(docid)
             forum_table += Markup("<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td>")
             if session['user_data']['login'] == post["username"]: #we session user is the same as poster
-                forum_table += Markup('<form action = "/delete" method = "post"> <tr> <td> <button value="Works?" type="submit" name="delete" class="btn btn-secondary">Delete</button> </td> </tr> </form>')
+                forum_table += Markup('<form action = "/delete" method = "post"> <tr> <td> <button value="Works" type="submit" name="delete" class="btn btn-secondary">Delete</button> </td> </tr> </form>')
                  #adds another column to the table with a delete button this is the code jared needs
         except Exception as e:
             print(e)
@@ -73,7 +73,7 @@ def posts_to_html():
 
 @app.route('/delete', methods=['POST'])
 def delete():
-    print(request.form["value"])
+    print(request.form["delete"])
 
     return render_template('home.html', past_posts = posts_to_html)
 
