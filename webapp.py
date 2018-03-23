67b7136b7f39bd0c683867613495a7cc43e0c624
Michael Moschitto <michaelmoschitto@gmail.com>
2018-03-23 09:38:16 -0700

    fixedspacing

--- webapp.py
+++ webapp.py
@@ -73,6 +73,7 @@ def posts_to_html():
     return forum_table
 
 @app.route('/delete', methods=['POST'])
+print("Entered Delete Method")
 def delete():
     # collection.deleteOne()
     print("delete")
